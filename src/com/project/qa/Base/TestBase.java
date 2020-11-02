d93c4dfe4e5d73d9650c72ac00014aadc292ca46
XE_Converter <sddas745@gmail.com>
2020-11-02 10:23:42 +0530

    Including Extent Report

--- src/com/project/qa/Base/TestBase.java
+++ src/com/project/qa/Base/TestBase.java
@@ -11,6 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.project.qa.util.TestUtil;
 
 public class TestBase {	
@@ -21,6 +25,9 @@ public class TestBase {
 	private static String password;
 	public static WebDriver driver;
 	public static String inputfileLocation;
+	public static ExtentHtmlReporter  htmlReporter;
+	public static ExtentReports extentReports;
+	public static ExtentTest extentTest;
 
 	public TestBase() throws FileNotFoundException {
 		prop=new Properties();
@@ -58,6 +65,24 @@ public class TestBase {
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(prop.getProperty("url"));
+	}
+	
+	public static void setExtentReport() {
+		htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/myReport.html");
+		htmlReporter.config().setDocumentTitle("Automation Report");
+		htmlReporter.config().setReportName("Functional TestCases");
+		htmlReporter.config().setTheme(Theme.DARK);
+		
+		extentReports=new ExtentReports();
+		extentReports.attachReporter(htmlReporter);
+		extentReports.setSystemInfo("TesterName", "Suresh Das");
+		extentReports.setSystemInfo("HostName", "LocalHost");
+		extentReports.setSystemInfo("OS", "Windows");
+		extentReports.setSystemInfo("Browser", "Chrome");
+	}
+	
+	public static void endReport() {
+		extentReports.flush();
 	}
 
 	public static String getUsername() {
